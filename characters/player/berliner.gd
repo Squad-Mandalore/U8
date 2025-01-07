99e56c6fab3026a6217439e195235c5f9f25de18
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-07 11:30:31 +0100

    feat: U8-35 use game template

--- characters/player/berliner.gd
+++ characters/player/berliner.gd
@@ -8,7 +8,7 @@ const SPEED: float = 102.0
 
 
 func _physics_process(_delta: float) -> void:
-    var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+    var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
     if direction.x < 0:
         _sprite.flip_h = true
