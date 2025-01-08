86e9b7b5bf6c9578823a40331b12ce621242f5cc
MrLappes <79363858+MrLappes@users.noreply.github.com>
2025-01-08 14:56:34 +0100

    press E to talk

--- characters/npcs/npc_base_class/npc.gd
+++ characters/npcs/npc_base_class/npc.gd
@@ -1,44 +1,81 @@
 extends CharacterBody2D
 class_name NPC
 
-enum State {IDLE, WALK}
+enum State {IDLE, WALK, TALK}
 const SPEED: float = 68.0
+
 var _direction: Vector2 = Vector2.ZERO
 var _current_state: State = State.IDLE
 var _slowdown_entities: int = 0
+var talking: bool = false
 
 @onready var _sprite = $AnimatedSprite2D
 @onready var _timer = $Timer
 @onready var slowdown_area = $SlowdownArea
 @onready var rich_text_label = $RichTextLabel
 
+signal npc_started_talking(npc: NPC)
+signal npc_stopped_talking(npc: NPC)
 
 func _ready() -> void:
     _new_state()
 
-
-func _physics_process(_delta: float) -> void:
-    if _direction.x < 0:
-        _sprite.flip_h = true
-    elif _direction.x > 0:
-        _sprite.flip_h = false
+func _physics_process(delta: float) -> void:
+    if _current_state == State.TALK:
+        # If talking, stay idle
+        _sprite.play("idle")
+        return
 
     var speed = SPEED
     if _slowdown_entities > 0:
         speed = SPEED / 2
 
-    if _direction:
+    if _direction.x < 0:
+        _sprite.flip_h = true
+    elif _direction.x > 0:
+        _sprite.flip_h = false
+
+    if _direction != Vector2.ZERO:
         velocity = _direction * speed
     else:
         velocity.x = move_toward(velocity.x, 0, speed)
         velocity.y = move_toward(velocity.y, 0, speed)
-    if velocity:
+
+    if velocity.length() > 0.1:
         _sprite.play("walk")
     else:
         _sprite.play("idle")
 
-    move_and_collide(velocity * _delta)
+    # Changed to move_and_collide instead of move_and_slide
+    # As slide thing caused the sticking to each other bug
+    move_and_collide(velocity * delta)
 
+func _input(event: InputEvent) -> void:
+    # If the label is visible and the player presses "talk", toggle talking
+    if event.is_action_pressed("talk") and rich_text_label.visible:
+        if talking:
+            _stop_talking()
+        else:
+            _start_talking()
+
+func _start_talking() -> void:
+    # This function sends a notification to Player script so that Player can talk to this npc.
+    talking = true
+    _timer.stop()
+    velocity = Vector2.ZERO
+    _direction = Vector2.ZERO
+    _current_state = State.TALK
+
+    emit_signal("npc_started_talking", self)
+
+func _stop_talking() -> void:
+    # This function needs to be called in order for player character to be able to walk again.
+    talking = false
+    _current_state = State.IDLE
+    # Resume normal walking after a small delay or immediately
+    _timer.start(1.0)
+
+    emit_signal("npc_stopped_talking", self)
 
 func _new_direction() -> void:
     _direction = Vector2(randi_range(-1, 1), randi_range(-1, 1))
@@ -53,18 +90,22 @@ func _new_state() -> void:
         _direction = Vector2.ZERO
         _timer.start(5.0)
 
-
 func _on_timer_timeout() -> void:
-    _new_state()
-
-
+    if _current_state != State.TALK:
+        _new_state()
 
 func _on_slowdown_area_body_entered(body: Node2D):
+    # If the Player enters this NPC's slowdown area, NPC speed is halved
     if body.is_in_group("Player"):
-        _slowdown_entities = _slowdown_entities + 1
+        _slowdown_entities += 1
         rich_text_label.visible = true
 
 func _on_slowdown_area_body_exited(body: Node2D):
+    # If the Player leaves, restore normal speed (if no more slowdown entities)
     if body.is_in_group("Player"):
-        _slowdown_entities = _slowdown_entities - 1
-        rich_text_label.visible = false
+        _slowdown_entities -= 1
+        if _slowdown_entities < 0:
+            _slowdown_entities = 0
+
+        if _slowdown_entities == 0:
+            rich_text_label.visible = false
--- characters/player/berliner.gd
+++ characters/player/berliner.gd
@@ -1,19 +1,20 @@
 class_name Player
 extends CharacterBody2D
 
-
 const SPEED: float = 102.0
 var _slowdown_entities: int = 0
 
 @onready var _animated_sprite_2d = $AnimatedSprite2D
+
 enum State {IDLE, WALK, TALK}
 var _current_state: State = State.IDLE
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
     if _current_state == State.TALK:
+        # If talking, skip movement
         return
-    var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 
+    var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
     var speed = SPEED
     if _slowdown_entities > 0:
         speed = SPEED / 2
@@ -23,47 +24,66 @@ func _physics_process(_delta: float) -> void:
     elif direction.x > 0:
         _animated_sprite_2d.flip_h = false
 
-    if direction:
+    if direction != Vector2.ZERO:
         velocity = direction * speed
+        switch_state(State.WALK)
     else:
         velocity.x = move_toward(velocity.x, 0, speed)
         velocity.y = move_toward(velocity.y, 0, speed)
-
-    if velocity:
-        switch_state(State.WALK)
-    else:
         switch_state(State.IDLE)
 
-    if velocity:
-        switch_state(State.WALK)
-    else:
-        switch_state(State.IDLE)
-
-    move_and_collide(velocity * _delta)
-
-
-func _unhandled_input(event: InputEvent):
-    if event.is_action_pressed("talk"):
-        switch_state(State.TALK)
+    move_and_collide(velocity * delta)
 
 func switch_state(new_state: State):
-    if _current_state == new_state and new_state == State.TALK:
-        new_state = State.IDLE
     if new_state != _current_state:
         _current_state = new_state
-        if _current_state == State.TALK:
-            _animated_sprite_2d.play("talk")
-        if _current_state == State.WALK:
-            _animated_sprite_2d.play("walk")
-        if _current_state == State.IDLE:
-            _animated_sprite_2d.play("idle")
+        match _current_state:
+            State.TALK:
+                _animated_sprite_2d.play("talk")
+            State.WALK:
+                _animated_sprite_2d.play("walk")
+            State.IDLE:
+                _animated_sprite_2d.play("idle")
 
 
+func _on_slowdown_area_body_entered(body: Node):
+    if body is NPC:
+        # If we meet an NPC, slow down the player
+        _slowdown_entities += 1
 
-func _on_slowdown_area_body_entered(body):
-    if body.is_in_group("NPC"):
-        _slowdown_entities = _slowdown_entities + 1
+        # Connect signals so we know when the NPC starts/stops talking
+        var npc: NPC = body
+        var start_talk_callable = Callable(self, "_on_npc_started_talking")
+        if not npc.is_connected("npc_started_talking", start_talk_callable):
+            npc.connect("npc_started_talking", start_talk_callable)
 
-func _on_slowdown_area_body_exited(body):
-    if body.is_in_group("NPC"):
-        _slowdown_entities = _slowdown_entities - 1
+        var stop_talk_callable = Callable(self, "_on_npc_stopped_talking")
+        if not npc.is_connected("npc_stopped_talking", stop_talk_callable):
+            npc.connect("npc_stopped_talking", stop_talk_callable)
+
+func _on_slowdown_area_body_exited(body: Node):
+    if body is NPC:
+        # If the NPC leaves, reduce slowdown count
+        _slowdown_entities -= 1
+        if _slowdown_entities < 0:
+            _slowdown_entities = 0
+
+        # Disconnect signals
+        var npc: NPC = body
+        var start_talk_callable = Callable(self, "_on_npc_started_talking")
+        if npc.is_connected("npc_started_talking", start_talk_callable):
+            npc.disconnect("npc_started_talking", start_talk_callable)
+
+        var stop_talk_callable = Callable(self, "_on_npc_stopped_talking")
+        if npc.is_connected("npc_stopped_talking", stop_talk_callable):
+            npc.disconnect("npc_stopped_talking", stop_talk_callable)
+
+func _on_npc_started_talking(npc: NPC):
+    # If NPC started talking, the player also enters TALK mode
+    switch_state(State.TALK)
+    print("NPC started talking to you")
+
+func _on_npc_stopped_talking(npc: NPC):
+    # If NPC stopped talking, the player reverts to IDLE (or WALK if moving)
+    switch_state(State.IDLE)
+    print("NPC stopped talking to you")
