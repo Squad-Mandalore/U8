26705ba939f7a0aa8ea9c75daeb4bb8208e5940d
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-16 00:02:07 +0100

    feat: U8-53 dynamic train length

--- scenes/game_scene/levels/train.gd
+++ scenes/game_scene/levels/train.gd
@@ -2,16 +2,30 @@ extends Node2D
 
 signal level_won
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-    pass # Replace with function body.
+const _LEFT_WIDTH: int = 329
+const _CENTER_WIDTH: int = 329
+const Y: int = 95
 
+var _rng = RandomNumberGenerator.new()
+var center: PackedScene  = preload("res://models/train/center.tscn")
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-    pass
-
-
-func _on_area_2d_body_entered(body:Node2D) -> void:
+func _on_area_2d_body_entered(_body: Node2D) -> void:
     level_won.emit()
+
+func _right_x(train_length: int) -> float:
+    assert(train_length >= 0)
+    return _CENTER_WIDTH * train_length + _LEFT_WIDTH
+
+func _center_x(train_length: int) -> float:
+    assert(train_length >= 0)
+    return _CENTER_WIDTH * train_length + _LEFT_WIDTH
+
+func _enter_tree() -> void:
+    var train_length: int = _rng.randi_range(0, 3)
+    for i in train_length:
+        var center_scene: Node2D = center.instantiate()
+        center_scene.position = Vector2(_center_x(i), Y)
+        self.add_child(center_scene)
+
+    ($Right as Node2D).position.x = _right_x(train_length)
 
