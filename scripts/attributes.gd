e3b1b8450944683a810f26ef81ba163c34ee3979
Moritz Bosch <anmoritzbosch@gmail.com>
2025-01-15 16:10:48 +0100

    [feat U8-45] initial stat implementation - doesnt work yet

---
+++ scripts/attributes.gd
@@ -0,0 +1,15 @@
+extends Node
+class_name Attributes
+
+@export var max_health: int
+@export var health: int
+@export var armor: int
+@export var initiative: int
+@export var dodge_chance: int
+@export var strength: int
+@export var coolness: int
+@export var attractiveness: int
+@export var intelligence: int
+@export var creativity: int
+# @export var radicality: int
+@export var luck: int
