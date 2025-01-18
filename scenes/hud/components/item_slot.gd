d0d4ba3627af797480be9d6420f3f0020692bec1
Markus Siegert <spongebobmarkus@gmail.com>
2025-01-18 11:59:09 +0100

    chore: restore my first drag deez nutz try

--- scenes/hud/components/item_slot.gd
+++ scenes/hud/components/item_slot.gd
@@ -33,3 +33,24 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
     if is_enabled():
         add_theme_stylebox_override("panel", load("res://assets/hud/item_slot_enabled.tres"))
+
+func _get_drag_data(at_position: Vector2) -> Variant:
+    var data = {}
+
+    data["drag_texture"] = self.get_child(0).texture
+
+    var drag_texture = TextureRect.new()
+    drag_texture.expand_mode = 1
+    drag_texture.texture = self.get_child(0).texture
+    drag_texture.size = Vector2(62, 62)
+
+    set_drag_preview(drag_texture)
+
+    return data
+
+func _can_drop_data(at_position: Vector2, data: Variant) -> bool:
+    return true
+
+func _drop_data(at_position: Vector2, data: Variant) -> void:
+    self.get_child(0).texture = data["drag_texture"]
+
