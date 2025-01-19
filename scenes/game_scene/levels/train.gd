53246f2f0df28a95c70a13302701191d5f525e51
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-19 19:37:55 +0100

    chore: changes to _ready

--- scenes/game_scene/levels/train.gd
+++ scenes/game_scene/levels/train.gd
@@ -23,15 +23,13 @@ func _center_x(train_length: int) -> float:
     assert(train_length >= 0)
     return _CENTER_WIDTH * train_length + _LEFT_WIDTH
 
-func _enter_tree() -> void:
+func _ready() -> void:
     var train_length: int = _rng.randi_range(0, 3)
     for i in train_length:
         var center_scene: Node2D = center.instantiate()
         center_scene.position = Vector2(_center_x(i), Y)
         self.add_child(center_scene)
-        
-    if not _right:
-        _right = $Right
+
     _right.position.x = _right_x(train_length)
 
 
