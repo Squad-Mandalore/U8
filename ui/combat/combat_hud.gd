8cd5ab7fe88f9b11e910bf03512a7cf8e8f63c61
Markus Siegert <spongebobmarkus@gmail.com>
2025-01-25 17:44:09 +0100

    fix: round descriptor

--- ui/combat/combat_hud.gd
+++ ui/combat/combat_hud.gd
@@ -3,23 +3,37 @@ extends CanvasLayer
 var attack_hover_scene = preload("res://ui/combat/subscenes/attack_hover.tscn")
 var attack_hover: AttackHover = null
 var enemy: Enemy
+var half_turn_counter: int
+var first_start: bool
+@onready var _feedback_box = %FeedbackBox
+@onready var _player_status_panel = %PlayerStatusPanel
+@onready var _enemy_status_panel = %EnemyStatusPanel
+@onready var _attack_swapper = %AttackSwapper
+@onready var _round_descriptor = %RoundDescriptor
+
 
 func _ready() -> void:
     SignalDispatcher.add_attack_hover.connect(add_attack_hover)
     SignalDispatcher.remove_attack_hover.connect(remove_attack_hover)
     SignalDispatcher.execute_attack.connect(execute_attack)
-    update_combat_hud()
+    half_turn_counter = 0
+    first_start = calculate_first_start()
+    if first_start:
+        _feedback_box.set_feedback("Deine Initiative ist höher, als die des Gegners.\nDu darfst starten.")
+    else:
+        _feedback_box.set_feedback("Der Gegner hat eine höhere Initiative als du.\nEr darf starten.")
+    _player_status_panel.stats = SourceOfTruth.stats
+    _enemy_status_panel.stats = enemy.stats
+    _attack_swapper.attacks = SourceOfTruth.get_all_attacks()
+    _round_descriptor.counter = 1
+
+func loop():
+    half_turn_counter += 1
+    if half_turn_counter % 2 == 0:
+        _round_descriptor.increment()
 
 func set_enemy(enemy: Enemy):
     self.enemy = enemy
-
-func update_combat_hud():
-    if calculate_first_start():
-        %FeedbackBox.set_feedback("Deine Initiative ist höher, als die des Gegners.\nDu darfst starten.")
-    %PlayerStatusPanel.stats = SourceOfTruth.stats
-    %EnemyStatusPanel.stats = enemy.stats
-    %AttackSwapper.attacks = SourceOfTruth.get_all_attacks()
-    %RoundDescriptor.counter = 1
 
 func add_attack_hover(position: Vector2, attack: Attack):
     attack_hover = attack_hover_scene.instantiate()
@@ -48,22 +62,26 @@ func take_damage(damage: int, stats: StatsSpecifier):
         SignalDispatcher.combat_exit.emit(get_parent())
 
 func execute_attack(attack: Attack, attacker: String):
-    var attacker_panel = get_node("%" + attacker + "StatusPanel")
+    loop()
+
+    var attacker_panel
     var defender_panel
     var enemy_stats
     if attacker == "Player":
-        defender_panel = get_node("%" + "Enemy" + "StatusPanel")
+        attacker_panel = _player_status_panel
+        defender_panel = _enemy_status_panel
         enemy_stats = enemy.stats
     else:
-        %RoundDescriptor.increment()
-        defender_panel = get_node("%" + "Player" + "StatusPanel")
+        attacker_panel = _enemy_status_panel
+        defender_panel = _player_status_panel
         enemy_stats = SourceOfTruth.stats
+
     for token_number in attack.token_number:
         attacker_panel.add_token(attack.token)
     take_damage(attack.damage, enemy_stats)
     attacker_panel.update_status_panel()
     defender_panel.update_status_panel()
-    %FeedbackBox.set_feedback(attacker + " hat " + str(attack.damage) + " Schaden gemacht.")
+    _feedback_box.set_feedback(attacker + " hat " + str(attack.damage) + " Schaden gemacht.")
     # TODO: needs better logic here
     await get_tree().create_timer(2).timeout
     if attacker == "Player":
