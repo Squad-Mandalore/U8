75164d1865206962b7681744b398e02b486c7029
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-27 12:28:11 +0100

    ci: minor text box tuning

--- ui/main_menu/main_menu.gd
+++ ui/main_menu/main_menu.gd
@@ -1,11 +1,18 @@
 extends MainMenu
 
 @onready var sub_title_label: Control = %SubTitleLabel
-var SUB_TITLE_TEXTS : Array[String] = ["Der realistischste Berliner U-Bahn Simulator", "Die wahre Berlin experience", "Scoot Scoot"]
+var sub_title_text : Array[String] = [
+    "Der realistischste Berliner U-Bahn Simulator",
+    "Die wahre Berlin experience",
+    "Scoot Scoot",
+    "Hier gilt Rauchverbot! Vielen Dank!",
+]
+const SEPERATOR: String = "             "
 
 func _ready() -> void:
     super._ready()
-    sub_title_label.text = SUB_TITLE_TEXTS[randi_range(0, SUB_TITLE_TEXTS.size() - 1)]
+    sub_title_text.shuffle()
+    sub_title_label.text = SEPERATOR.join(sub_title_text)
 
 func load_game_scene():
     GameState.start_game()
