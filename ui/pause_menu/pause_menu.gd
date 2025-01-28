d79f8c7366cf53d196356017002b8bb0968d6bce
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-28 10:51:00 +0100

    chore: save on quite to main menu

--- ui/pause_menu/pause_menu.gd
+++ ui/pause_menu/pause_menu.gd
@@ -3,3 +3,7 @@ extends PauseMenu
 func _on_confirm_exit_confirmed():
     GlobalState.save()
     get_tree().quit()
+
+func _on_confirm_main_menu_confirmed():
+    GlobalState.save()
+    _load_scene(main_menu_scene)
