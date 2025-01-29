9f6ed8c9da714c45ec49210d6b838879d2dbbdb3
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-29 16:40:18 +0100

    feat: weapon info in inventory

--- ui/inventory/subscenes/item_info.gd
+++ ui/inventory/subscenes/item_info.gd
@@ -1,6 +1,8 @@
 extends VBoxContainer
 
 var new_stat_row_scene = preload("res://ui/inventory/subscenes/item_info_stat_row.tscn")
+@onready var _desription_box = $HBoxContainer3
+@onready var _attack_container = $AttackContainer
 
 func update_item_info(item: Item, is_shop_slot: bool):
     set_item_name(item)
@@ -8,6 +10,8 @@ func update_item_info(item: Item, is_shop_slot: bool):
     set_item_price(item, is_shop_slot)
     Utils.remove_all_children(%StatVBox)
     set_stats(item)
+    if item is Weapon:
+        _handle_weapon(item)
 
 func set_item_name(item: Item):
     (%ItemNameLabel as Label).text = item.name
@@ -36,3 +40,11 @@ func set_stats(item: Item):
 func get_ck3_progress_bar() -> TextureProgressBar:
     %Ck3ProgressBar.value = 0
     return %Ck3ProgressBar
+
+func _handle_weapon(item: Weapon):
+    _desription_box.hide()
+    _attack_container.show()
+    %AttackSwapper.max_attacks = 2 # this line MUST be executed before the attacks are set
+    %AttackSwapper.attacks = item.attacks
+    %AttackSwapper.change_margin(Vector4(0,0,0,0))
+
