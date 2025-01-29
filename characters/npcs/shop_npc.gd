c7e6f1c38e7c95971a0ccc0b7edf758a1fbd1a27
BB223 <79644542+BB223@users.noreply.github.com>
2025-01-29 17:53:23 +0100

    shop name

--- characters/npcs/shop_npc.gd
+++ characters/npcs/shop_npc.gd
@@ -4,7 +4,6 @@ class_name ShopNpc
 enum SubState { NONE, SHOP }
 var _sub_state = SubState.NONE
 @export var shop_inventory: Array[Item]
-@export var shop_name: String
 var shop_hud: Control
 var shop_info_panel: Control
 
@@ -12,7 +11,7 @@ func open_shop(shop_canvas: CanvasLayer):
     _sub_state = SubState.SHOP
     shop_hud = shop_canvas.shop_hud
     shop_info_panel = shop_canvas.info_panel
-    shop_hud.reload(true, shop_inventory, _sprite.sprite_frames)
+    shop_hud.reload(true, shop_inventory, _name, _sprite.sprite_frames)
 
 func close_shop():
     var _sub_state = SubState.NONE
