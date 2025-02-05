339e8216221f68d33420cbe0f28e142e81538d7f
Markus Siegert <spongebobmarkus@gmail.com>
2025-02-06 00:17:24 +0100

    fix: progress bar and olaf tutorial enemy

--- characters/npcs/politician/politician.gd
+++ characters/npcs/politician/politician.gd
@@ -1,1 +1,17 @@
-extends WalkingNpc
+extends Enemy
+
+func start_talking() -> void:
+    _current_state = State.TALK
+    enable_outline(Color(0, 0, 1, 1))
+    start_combat()
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+    if _sprite.animation == "idle2":
+        _sprite.play("idle")
+    elif _sprite.animation == "battle_intro":
+        _sprite.play("battle_idle")
+
+func _on_animated_sprite_2d_animation_looped() -> void:
+    if _sprite.animation == "idle":
+        if randi() % 5 == 3:
+            _sprite.play("idle2")
