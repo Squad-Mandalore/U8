9f53d41154288e7aa43876e9b9a65ea42ea5eee3
Markus Siegert <spongebobmarkus@gmail.com>
2025-02-11 13:57:41 +0100

    chore: remove unecessary start_combat calls

--- characters/npcs/lifeguard/lifeguard.gd
+++ characters/npcs/lifeguard/lifeguard.gd
@@ -18,7 +18,6 @@ func enable_outline(color : Color = Color(1, 0, 0, 1)) -> void:
 func start_talking() -> void:
     _current_state = State.TALK
     enable_outline(Color(0, 0, 1, 1))
-    start_combat()
 
 func _on_animated_sprite_2d_animation_finished() -> void:
     if _sprite.animation == "idle2":
--- characters/npcs/politician/politician.gd
+++ characters/npcs/politician/politician.gd
@@ -3,7 +3,6 @@ extends Enemy
 func start_talking() -> void:
     _current_state = State.TALK
     enable_outline(Color(0, 0, 1, 1))
-    start_combat()
 
 func _on_animated_sprite_2d_animation_finished() -> void:
     if _sprite.animation == "idle2":
