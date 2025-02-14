c3dd723fab879a3fc460ceadfb0fc93ffb008134
MrLappes <79363858+MrLappes@users.noreply.github.com>
2025-02-14 16:14:08 +0100

    feat: We have animations boys

--- characters/npcs/seated/seated_npc.gd
+++ characters/npcs/seated/seated_npc.gd
@@ -1,46 +1,69 @@
 extends Npc
 class_name SeatedNpc
 
-@onready var _sprite_2d: Sprite2D = $Sprite2D
-@export var _textures_path : String = "res://characters/npcs/seated/pointed_away/assets/"
+var _animation_name: String = ""  
+var _base_animation: String = ""  
+var _animation_variants: Array = []  # Stores all available variants
 
 func subclass_ready():
-    if randi_range(0,5) == 1:
+    if randi_range(0, 5) == 1:
         queue_free()
-    if randi_range(0,1) == 1:
-        _sprite_2d.flip_h = true
-    var _walker: DirAccess = DirAccess.open(_textures_path)
-    var _custom_character_textures : Array[String] = []
-    _custom_character_textures.assign(_walker.get_files())
-    _custom_character_textures.filter(func(file_name): return not "import" in file_name)
-    var _texture_id : int = randi_range(0, _custom_character_textures.size() - 1)
-    var _formated_path = "{}{}".format([_textures_path, _custom_character_textures[_texture_id]], "{}")
-    _sprite_2d.texture = load(_formated_path)
-    _current_state = State.IDLE
-    var _male = "hombre" in _formated_path
-    var _gender = "Male" if _male else "Female"
-    _name = NameGenerator.get_random_name(_gender)
-    return true
+        return 
 
+    if randi_range(0, 1) == 1:
+        _sprite.flip_h = true
+
+    var animation_names = _sprite.sprite_frames.get_animation_names()
+    var unique_animations = {}
+
+    # Extract unique base animation names
+    for anim in animation_names:
+        var base_name = anim.rstrip("0123456789")
+        if base_name in unique_animations:
+            unique_animations[base_name].append(anim)
+        else:
+            unique_animations[base_name] = [anim]
+    var base_keys = unique_animations.keys()
+    _base_animation = base_keys[randi_range(0, base_keys.size() - 1)]
+    _animation_variants = unique_animations[_base_animation]
+
+    # Pick the default animation:
+    if _base_animation + "1" in _animation_variants:
+        _animation_name = _base_animation + "1"  # Use X1 as base animation
+    else:
+        _animation_name = _base_animation
+
+    _sprite.play(_animation_name)
 
 func make_space(body : Node2D) -> void:
     return
 
-func enable_outline(color : Color = Color(0, 1, 0, 1)) -> void:
-    # Create and assign a ShaderMaterial with the given outline shader
-    if outline_shader:
-        var mat = ShaderMaterial.new()
-        mat.shader = outline_shader
-        # Adjust parameters as needed
-        mat.set_shader_parameter("outline_thickness", 0.5)
-        mat.set_shader_parameter("outline_color", color)
-        _sprite_2d.material = mat
-    else:
-        # No shader assigned
-        _sprite_2d.material = null
-
 func set_player_nearby(is_player_nearby : bool):
     _player_nearby = is_player_nearby
 
-func disable_outline() -> void:
-    _sprite_2d.material = null
+func _on_animated_sprite_2d_animation_looped() -> void:
+    if _animation_variants.size() == 1:
+        return  # No variations, do nothing
+
+    # If currently on X1 animation, randomly switch
+    if _animation_name.ends_with("1"):
+        if randi_range(1, 10) == 1:
+            var new_animation = _pick_random_variant()
+            if new_animation:
+                _animation_name = new_animation
+                _sprite.play(_animation_name)
+    else:
+        # Always return to `X1` after playing any variant
+        _animation_name = _base_animation + "1"
+        _sprite.play(_animation_name)
+
+func _pick_random_variant() -> String:
+    var possible_variations = _animation_variants.duplicate()
+
+    if _base_animation + "1" in possible_variations:
+        possible_variations.erase(_base_animation + "1")
+
+    if possible_variations.size() > 0:
+        return possible_variations[randi_range(0, possible_variations.size() - 1)]
+    
+    return _animation_name
