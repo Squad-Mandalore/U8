1fde4578a8a97c13f98c249bc3c39f723ea11948
BB223 <79644542+BB223@users.noreply.github.com>
2025-02-24 20:37:33 +0100

    ci: andreas dialogue

--- characters/npcs/andreas/andreas.gd
+++ characters/npcs/andreas/andreas.gd
@@ -5,6 +5,9 @@ var dishonest_brothers_quest: QuestEntry = Questomania.quest_dict[DishonestBroth
 const CHRIS_ROBBERY_ID = 1
 const ANDREAS_REMEMBERS_ID = 2
 const ANDREAS_ROBBERY_ID = 3
+var _chris_robbery_quest: QuestEntry = dishonest_brothers_quest.get_subquest(CHRIS_ROBBERY_ID)
+var _andreas_remembers_quest: QuestEntry = dishonest_brothers_quest.get_subquest(ANDREAS_REMEMBERS_ID)
+var _andreas_robbery_quest: QuestEntry = dishonest_brothers_quest.get_subquest(ANDREAS_ROBBERY_ID)
 
 const WINNING_MONEY = 10
 
@@ -12,17 +15,20 @@ const WINNING_MONEY = 10
 var dialogue = preload("res://characters/npcs/andreas/assets/andreas_robbery.dialogue")
 
 func _enter_tree() -> void:
-    if not dishonest_brothers_quest.get_subquest(CHRIS_ROBBERY_ID).is_completed() or SourceOfTruth.chance(40):
+    if not _chris_robbery_quest.is_completed() or SourceOfTruth.chance(40):
         queue_free()
         return
 
 func set_player_nearby(is_player_nearby : Player):
     _player_nearby = is_player_nearby
-    if not _player_nearby:
+    if not _player_nearby or not dishonest_brothers_quest.is_active():
         return
 
-    if dishonest_brothers_quest.get_subquest(ANDREAS_REMEMBERS_ID).is_active():
-        start_combat()
+    if _andreas_remembers_quest.is_active():
+        start_talking()
+        _player_nearby._start_scripted_talking(self)
+        DialogueManager.show_dialogue_balloon(dialogue, "remembers", [self])
+        DialogueManager.dialogue_ended.connect(_dialogue_ended)
     else:
         start_robbing()
 
@@ -41,24 +47,19 @@ func start_robbing():
 
 func _dialogue_ended(_resource):
     DialogueManager.dialogue_ended.disconnect(_dialogue_ended)
-    dishonest_brothers_quest.get_subquest(CHRIS_ROBBERY_ID).set_completed(true)
     stop_talking()
     _player_nearby._stop_talking(self)
-    start_combat()
 
 func _robbing(percent: float):
     var money = floor(SourceOfTruth.balance * percent)
     print("Andreas stole %d Euronen" % money)
     SourceOfTruth.balance_changed(-money)
-    dishonest_brothers_quest.get_subquest(ANDREAS_ROBBERY_ID).set_completed(true)
-    dishonest_brothers_quest.get_subquest(ANDREAS_ROBBERY_ID).set_active(false)
-    dishonest_brothers_quest.set_completed(true)
+    _quest_robbery_complete()
 
 func _impressing():
     # TODO: sonnenbrille geben
-    dishonest_brothers_quest.get_subquest(ANDREAS_ROBBERY_ID).set_completed(true)
-    dishonest_brothers_quest.get_subquest(ANDREAS_ROBBERY_ID).set_active(false)
-    dishonest_brothers_quest.set_completed(true)
+    _quest_robbery_complete()
+
 
 func _let_robbing_happen():
     dishonest_brothers_quest.set_meta("red", true)
@@ -67,10 +68,22 @@ func _let_robbing_happen():
 func fight_lost():
     print("Player won %d Euronen" % WINNING_MONEY)
     SourceOfTruth.balance_changed(WINNING_MONEY)
-    if dishonest_brothers_quest.get_subquest(ANDREAS_REMEMBERS_ID).is_active():
-        dishonest_brothers_quest.get_subquest(ANDREAS_REMEMBERS_ID).set_completed(true)
-        dishonest_brothers_quest.get_subquest(ANDREAS_REMEMBERS_ID).set_active(false)
+    if _andreas_remembers_quest.is_active():
+        _quest_remembers_complete()
     else:
-        dishonest_brothers_quest.get_subquest(ANDREAS_ROBBERY_ID).set_completed(true)
-        dishonest_brothers_quest.get_subquest(ANDREAS_ROBBERY_ID).set_active(false)
+        _quest_robbery_complete()
+
+func _quest_robbery_complete():
+    _andreas_robbery_quest.set_active(false)
+    _andreas_robbery_quest.set_completed(true)
+    _quest_dishones_brothers_complete()
+
+func _quest_remembers_complete():
+    _andreas_remembers_quest.set_active(false)
+    _andreas_remembers_quest.set_completed(true)
+    _quest_dishones_brothers_complete()
+
+func _quest_dishones_brothers_complete():
+    dishonest_brothers_quest.set_active(false)
     dishonest_brothers_quest.set_completed(true)
+
