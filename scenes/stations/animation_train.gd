a43514dc1a43ac2a15107c7da4cd1d5e7609cc4c
Markus Siegert <spongebobmarkus@gmail.com>
2025-02-24 01:02:29 +0100

    fix: collisions of stations and train

--- scenes/stations/animation_train.gd
+++ scenes/stations/animation_train.gd
@@ -6,15 +6,10 @@ class_name AnimatedTrain
 @onready var _right: AnimatedWagons = $Right
 
 var activated: bool = false
-var doors_open: bool = false
-var first_time: bool = true 
-signal change_collisions(deactivate)
 
 func activate_doors() -> void:
-    if not activated:
-        activated = true
-        first_time = true
-        _open_all_doors()
+    activated = true
+    _open_all_doors()
 
 func deactivate_doors() -> void:
     activated = false
@@ -25,29 +20,15 @@ func _on_area_2d_body_entered(_body: Node2D) -> void:
         _open_all_doors()
 
 func _on_area_2d_body_exited(_body: Node2D) -> void:
-    if first_time:
-        first_time = false
-        return
-    
     if activated:
         _close_all_doors()
 
 func _open_all_doors() -> void:
-    doors_open = true
     _center.open_doors()
     _left.open_doors()
     _right.open_doors()
 
 func _close_all_doors() -> void:
-    doors_open = false
     _center.close_doors()
     _left.close_doors()
     _right.close_doors()
-
-func _on_wagon_doors_fully_opened() -> void:
-    doors_open = true
-    change_collisions.emit(true) 
-
-func _on_wagon_doors_start_closing() -> void:
-    doors_open = false
-    change_collisions.emit(false)
