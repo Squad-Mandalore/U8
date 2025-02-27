2a068cd4bd23ec989b1a12d5b7916364da7c4306
Lucas Buchholz <lucas.buchholz1@gmail.com>
2025-02-27 13:56:33 +0100

    fix: ident2

--- scenes/stations/01_wittenau/subscenes/tower.gd
+++ scenes/stations/01_wittenau/subscenes/tower.gd
@@ -3,7 +3,7 @@ extends StaticBody2D
 
 
 func _on_area_2d_body_exited(_body: Node2D) -> void:
-	modulate.a = 1
+    modulate.a = 1
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
-	modulate.a = 0.5
+    modulate.a = 0.5
