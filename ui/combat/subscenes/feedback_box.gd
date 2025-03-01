70dfd5186bd49f0d04afefb5dcc28719b030760d
Marvin <hofmann.marvin03@gmail.com>
2025-03-01 18:13:25 +0100

    feat: add typewriter effect

--- ui/combat/subscenes/feedback_box.gd
+++ ui/combat/subscenes/feedback_box.gd
@@ -1,4 +1,9 @@
 extends Control
 
+@onready var rich_text_label = $PanelContainer/MarginContainer/FeedbackLabel
+@onready var animation_player = $PanelContainer/MarginContainer/AnimationPlayer
+
 func set_feedback(feedback: String):
-    %FeedbackLabel.text = feedback
+	animation_player.stop()
+	rich_text_label.text = feedback
+	animation_player.play("typewriter")
