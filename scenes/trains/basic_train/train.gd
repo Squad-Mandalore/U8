0cb6d9fe4b96c967b94dc011386538727d3caee4
Markus Siegert <spongebobmarkus@gmail.com>
2025-03-01 19:55:13 +0100

    chore: only on first station

--- scenes/trains/basic_train/train.gd
+++ scenes/trains/basic_train/train.gd
@@ -14,7 +14,6 @@ const Y: int = 95
 @export var combat_floor: Texture2D
 @onready var animation_player = $AnimationPlayer
 
-
 var _rng = RandomNumberGenerator.new()
 var center: PackedScene  = preload("res://scenes/trains/basic_train/subscenes/center.tscn")
 
@@ -40,7 +39,10 @@ func _ready() -> void:
     _right.position.x = _right_x(train_length)
     _spawn_ticket_inspector()
     SignalDispatcher.sound_music.emit("train")
-    animation_player.play("olaf_entrance")
+    if GameState.get_current_station() == 0:
+        animation_player.play("olaf_entrance")
+    else:
+        $Politiker.queue_free()
 
 
 func _spawn_ticket_inspector():
