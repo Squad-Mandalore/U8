793394611838b6d74943dab22224acfd7b37b209
BB223 <79644542+BB223@users.noreply.github.com>
2025-03-04 14:29:18 +0100

    wittenau train animation

--- scenes/stations/01_wittenau/wittenau.gd
+++ scenes/stations/01_wittenau/wittenau.gd
@@ -4,6 +4,9 @@ signal train_enter
 
 @onready var animation_player = $AnimationPlayer
 @onready var olaf = %OlafScholz
+@onready var animation_train = $Node2D/AnimationTrain
+@onready var animation_train_collision = $Node2D/AnimationTrain/CollisionShape2D
+@onready var player = $Node2D/Berliner
 @export var combat_background: Texture2D
 @export var combat_background_left: Texture2D
 @export var combat_floor: Texture2D
@@ -11,6 +14,8 @@ var train_accessable = false
 
 func _ready() -> void:
     SignalDispatcher.sound_music.emit("station")
+    animation_train.activate_doors()
+    animation_train_collision.disabled = false
 
 func _on_area_2d_body_entered(_body: Node2D) -> void:
     if train_accessable:
@@ -23,6 +28,13 @@ func _on_animation_player_animation_finished(anim_name:StringName) -> void:
         "olaf_initial_battle":
             olaf.start_combat()
             train_accessable = true
-        "olaf_step_aside":
+        "train_leave":
             train_enter.emit()
+
+func _on_animation_train_body_entered(_body: Node2D) -> void:
+    animation_train.deactivate_doors()
+    animation_train_collision.call_deferred("set_disabled", true)
+    player.hide()
+    player.speed_multiplier = 0.0
+    animation_player.play("train_leave")
 
