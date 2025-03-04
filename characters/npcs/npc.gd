6aeba1177645e74f72420604fe0d3751cd812e3f
Markus Siegert <spongebobmarkus@gmail.com>
2025-03-04 17:00:46 +0100

    chore: comment cleanup

--- characters/npcs/npc.gd
+++ characters/npcs/npc.gd
@@ -17,7 +17,6 @@ func _ready() -> void:
     if _random_name:
         _name = NameGenerator.get_random_name(_gender)
 
-
 func start_talking() -> void:
     _current_state = State.TALK
     enable_outline(Color(0, 0, 1, 1))
@@ -90,37 +89,6 @@ func get_valid_directions() -> Array:
         if not is_position_obstructed(check_pos):
             valid_dirs.append(dir)
     return valid_dirs
-
-# # function for schackeline, perhaps use for player if stuck as well
-# func make_space() -> void:
-#     # Moves the npc out of the center of the train
-#     position += pick_valid_direction() * 5
-
-# func is_position_obstructed(target_position: Vector2) -> bool:
-#     var world = get_world_2d()
-#     if !world:
-#         return false
-#     var space_state = world.direct_space_state
-#     var query = PhysicsRayQueryParameters2D.create(global_position, target_position)
-#     var result = space_state.intersect_ray(query)
-#     return result.size() > 0
-
-# # Helper to find an unobstructed direction
-# func pick_valid_direction() -> Vector2:
-#     var directions = [
-#         Vector2(0, -1),
-#         Vector2(0, 1),
-#         Vector2(-1, 0),
-#         Vector2(1, 0)
-#     ]
-#     directions.shuffle()
-
-#     for dir in directions:
-#         var check_pos = global_position + dir * 24
-#         if not is_position_obstructed(check_pos):
-#             return dir
-
-#     return Vector2.ZERO
 
 func start_animation(animation: String):
     _sprite.animation = animation
