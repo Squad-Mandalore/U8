ac94db5c92f3201c1f9823183638b0cdc696050c
flockean <lucas.buchholz1@gmail.com>
2025-03-08 00:39:46 +0100

    ci: boss area

--- scenes/stations/18_moritzplatz/moritzplatz.gd
+++ scenes/stations/18_moritzplatz/moritzplatz.gd
@@ -3,5 +3,5 @@ extends Station
 @onready var moritz = %Moritz
 
 func _on_boss_area_body_exited(_body: Node2D) -> void:
-	moritz.start_combat()
-	$BossArea.queue_free()
+    moritz.start_combat()
+    $BossArea.queue_free()
