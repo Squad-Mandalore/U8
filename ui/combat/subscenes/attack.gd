d15657c7396125521dce7af981f07206888942a4
Markus Siegert <spongebobmarkus@gmail.com>
2025-03-10 19:25:35 +0100

    ci: step stuck i am bro

--- ui/combat/subscenes/attack.gd
+++ ui/combat/subscenes/attack.gd
@@ -47,7 +47,7 @@ func update_icon(effect: String):
 
 func _on_panel_container_gui_input(event: InputEvent) -> void:
     if event is InputEventMouseButton:
-            if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-                # TODO: play attack animation and hide hud
-                SignalDispatcher.attack_swapper_toggle.emit(false)
-                SignalDispatcher.execute_attack.emit(attack, "Spieler", "Enemy")
+        if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+            # TODO: play attack animation and hide hud
+            SignalDispatcher.attack_swapper_toggle.emit(false)
+            SignalDispatcher.execute_attack.emit(attack, "Spieler", "Enemy")
