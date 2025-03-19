bd2d924ccd1421452a0762575c0d43eb02a47192
BB223 <79644542+BB223@users.noreply.github.com>
2025-03-19 18:21:30 +0100

    fix: wrong button pressed

--- ui/subscenes/button_hud.gd
+++ ui/subscenes/button_hud.gd
@@ -6,9 +6,6 @@ extends Button
 func _ready() -> void:
     set_key_icon(action_event_key)
 
-func is_activated_by_shortcut(action: String) -> bool:
-    return Input.is_action_just_pressed(action)
-
 func set_key_icon(action: StringName) -> void:
     var talk_action_key = InputMap.action_get_events(action)[0].as_text()[0]
     interaction_key_label.text = talk_action_key
