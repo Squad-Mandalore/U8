e826d3bf2d071b8fc412e587824a9a7d5170ed21
Ronny Vater <rv48@hotmail.de>
2025-03-23 00:56:55 +0100

    some style changes

--- scenes/dialogue/subscenes/chat_message.gd
+++ scenes/dialogue/subscenes/chat_message.gd
@@ -6,11 +6,12 @@ func set_side(is_left: bool):
     else:
         add_theme_stylebox_override("normal", preload("res://scenes/dialogue/assets/chat_ui_right.tres"))
 
-func add_new_text(text: String):
-    self.text = text
+func add_new_text(new_text: String):
+    self.text = new_text
     var parent_width = 921
     var font = get_theme_font("font")
-    var content_width = font.get_string_size(text).x
+    var content_width = font.get_string_size(new_text).x
+    # TODO: custom minimum size ist hier evtl. nicht die beste Wahl
     custom_minimum_size.x = min(content_width, parent_width)
     print(parent_width)
     print(content_width)
