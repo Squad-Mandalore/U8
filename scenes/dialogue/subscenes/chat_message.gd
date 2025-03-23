af1fa45f5dd0ee5a631b298a5c1a38c487964ea6
Ronny Vater <rv48@hotmail.de>
2025-03-23 18:21:03 +0100

    fix: made labels dynamically adjust properly

--- scenes/dialogue/subscenes/chat_message.gd
+++ scenes/dialogue/subscenes/chat_message.gd
@@ -10,6 +10,24 @@ func add_new_text(new_text: String):
     self.text = new_text
     var parent_width = 921
     var font = get_theme_font("font")
+    
+    autowrap_mode = TextServer.AUTOWRAP_WORD_SMART
+    
     var content_width = font.get_string_size(new_text).x
-    # TODO: custom minimum size ist hier evtl. nicht die beste Wahl
-    custom_minimum_size.x = min(content_width, parent_width)
+    
+    if content_width < parent_width * 0.6:
+        size_flags_horizontal = SIZE_SHRINK_BEGIN
+        custom_minimum_size.x = min(content_width + 50, parent_width * 0.6)
+    else:
+        size_flags_horizontal = SIZE_FILL
+        custom_minimum_size.x = parent_width * 0.7
+    
+    size = Vector2.ZERO
+    custom_minimum_size.y = 0
+    
+    call_deferred("_update_layout")
+
+func _update_layout():
+    if get_parent():
+        get_parent().queue_sort()
+    queue_redraw()
