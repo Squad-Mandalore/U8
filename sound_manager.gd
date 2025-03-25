92a2d2dadb4d310f31d723d31d5f5b317300bfc2
BB223 <79644542+BB223@users.noreply.github.com>
2025-03-25 18:25:22 +0100

    fix: audio bus

--- sound_manager.gd
+++ sound_manager.gd
@@ -11,6 +11,7 @@ var button_clicked_effect = preload("res://assets/sounds/effects/button_clicked_
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     SignalDispatcher.sound_effect.connect(_on_sound_effect)
+    self.bus = "SFX"
 
 func play_sound_effect(sound: AudioStream, volume_db: float = 0) -> void:
     if sound:
